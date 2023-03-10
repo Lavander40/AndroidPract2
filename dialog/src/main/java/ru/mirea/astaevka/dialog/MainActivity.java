4dcaab8528b5c2fcb32f0e05f81d892422a110da
Lavander40 <a.kirill.03@mail.ru>
2023-03-10 14:04:47 +0300

    practice complete

--- dialog/src/main/java/ru/mirea/astaevka/dialog/MainActivity.java
+++ dialog/src/main/java/ru/mirea/astaevka/dialog/MainActivity.java
@@ -3,8 +3,13 @@ package ru.mirea.astaevka.dialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.DatePicker;
@@ -18,21 +23,9 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     Calendar dateAndTime = Calendar.getInstance();
-    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
-            dateAndTime.set(Calendar.MINUTE, minute);
-            setInitialTime(view);
-        }
-    };
-    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            dateAndTime.set(Calendar.YEAR, year);
-            dateAndTime.set(Calendar.MONTH, monthOfYear);
-            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            setInitialDate(view);
-        }
-    };
+    String currentDateTime;
+    ProgressDialog pd;
+    Handler h;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +36,6 @@ public class MainActivity extends AppCompatActivity {
     public void onClickShowDialog(View view){
         AlertDialogFragment dialogFragment = new AlertDialogFragment();
         dialogFragment.show(getSupportFragmentManager(), "mirea");
-    }
-
-    public void onTimeShowDialog(View view){
-        new TimePickerDialog(MainActivity.this, t,
-                dateAndTime.get(Calendar.HOUR_OF_DAY),
-                dateAndTime.get(Calendar.MINUTE), true)
-                .show();
-    }
-
-    public void onDateShowDialog(View v) {
-        new DatePickerDialog(MainActivity.this, d,
-                dateAndTime.get(Calendar.YEAR),
-                dateAndTime.get(Calendar.MONTH),
-                dateAndTime.get(Calendar.DAY_OF_MONTH))
-                .show();
     }
 
     public void onOkClicked() {
@@ -73,22 +51,55 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_LONG).show();
     }
 
-    public void setInitialTime(View view){
-        Snackbar.make(view, DateUtils.formatDateTime(this,
-                dateAndTime.getTimeInMillis(),
-                DateUtils.FORMAT_SHOW_TIME), Snackbar.LENGTH_LONG)
+    public void onClickedTimeDialog(View view)  {
+        new TimePickerDialog(MainActivity.this, t,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
                 .show();
     }
 
-    public void setInitialDate(View view){
-        Snackbar.make(view, DateUtils.formatDateTime(this,
-                dateAndTime.getTimeInMillis(),
-                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR), Snackbar.LENGTH_LONG)
+    public void onClickedDateDialog(View view)  {
+        new DatePickerDialog(MainActivity.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
                 .show();
     }
 
-    public void onProgressClicked(View view){
-        Snackbar.make(view, "wow", Snackbar.LENGTH_LONG)
-                .show();
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
+
+    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+            setInitialDateTime();
+            //Snackbar.make(view, hourOfDay+":"+minute, Snackbar.LENGTH_LONG).show();
+        }
+    };
+
+    private void setInitialDateTime() {
+        currentDateTime = DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME);
+    }
+
+    public void onProgressClicked(View v) {
+        pd = new ProgressDialog(this);
+        pd.setTitle("Title");
+        pd.setMessage("progressing");
+        pd.setButton(Dialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Snackbar.make(v, "progress complete", Snackbar.LENGTH_LONG).show();
+            }
+        });
+        pd.show();
     }
 }
